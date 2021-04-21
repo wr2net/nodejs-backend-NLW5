bcc754d6444284d8f7e39ccc41d7d1eb7ebc8c41
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2021-04-21 17:49:13 -0300

    Aula 03 - In Orbit

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,53 @@
+import {getCustomRepository, Repository} from "typeorm"
+import {SettingsRepository} from "../repositories/SettingsRepository"
+import {Setting} from "../entities/Setting";
+
+interface ISettingsCreate {
+    chat: boolean
+    username: string
+}
+
+class SettingsService {
+    
+    private settingsRepository: Repository<Setting>
+
+    constructor() {
+        this.settingsRepository = getCustomRepository(SettingsRepository)
+    }
+    
+    async create({chat, username} : ISettingsCreate) {
+
+        const userAlreadyExists = await this.settingsRepository.findOne({
+            username
+        })
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists!")
+        }
+
+        const settings = this.settingsRepository.create({
+            chat,
+            username
+        })
+
+        await this.settingsRepository.save(settings)
+
+        return settings
+    }
+
+    async list() {
+        const list = await this.settingsRepository.find()
+        return list
+    }
+
+    async findById(id: string) {
+
+        const show = await this.settingsRepository.findOne({
+            id
+        })
+
+        return show
+    }
+}
+
+export {SettingsService}
---
+++ src/services/UsersService.ts
@@ -0,0 +1,47 @@
+import {getCustomRepository, Repository} from "typeorm";
+import {UsersRepository} from "../repositories/UsersRepository";
+import {User} from "../entities/User";
+
+class UsersService {
+
+    private usersRepository: Repository<User>
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository)
+    }
+
+    async create(email: string) {
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        })
+
+        if (userExists) {
+            return userExists
+        }
+
+        const user = this.usersRepository.create({
+            email
+        })
+
+        await this.usersRepository.save(user)
+
+        return user
+    }
+
+    async list() {
+        const list = await this.usersRepository.find()
+        return list
+    }
+
+    async findById(id: string) {
+
+        const show = await this.usersRepository.findOne({
+            id
+        })
+
+        return show
+    }
+}
+
+export {UsersService}
